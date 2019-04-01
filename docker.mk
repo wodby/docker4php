94e25216d26d219c27c68cf606867e9e94795c1a
Chingis Sandanov <chingis@wodby.com>
2019-04-01 16:54:22 +0700

    Add make help, make start commands. Allow pruning individual services

--- docker.mk
+++ docker.mk
@@ -4,27 +4,49 @@ include .env
 
 default: up
 
+## help	:	Print commands help.
+help : docker.mk
+	@sed -n 's/^##//p' $<
+
+## up	:	Start up containers.
 up:
 	@echo "Starting up containers for for $(PROJECT_NAME)..."
 	docker-compose pull
 	docker-compose up -d --remove-orphans
 
+## down	:	Stop containers.
 down: stop
 
+## start	:	Start containers without updating.
+start:
+	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
+	@docker-compose start
+
+## stop	:	Stop containers.
 stop:
 	@echo "Stopping containers for $(PROJECT_NAME)..."
 	@docker-compose stop
 
+## prune	:	Remove containers and their volumes.
+##		You can optionally pass an argument with the service name to prune single container
+##		prune mariadb	: Prune `mariadb` container and remove its volumes.
+##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
 prune:
 	@echo "Removing containers for $(PROJECT_NAME)..."
-	@docker-compose down -v
+	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
 
+## ps	:	List running containers.
 ps:
 	@docker ps --filter name='$(PROJECT_NAME)*'
 
+## shell	:	Access `php` container via shell.
 shell:
 	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='$(PROJECT_NAME)_php' --format "{{ .ID }}") sh
 
+## logs	:	View containers logs.
+##		You can optinally pass an argument with the service name to limit logs
+##		logs php	: View `php` container logs.
+##		logs nginx php	: View `nginx` and `php` containers logs.
 logs:
 	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
 
