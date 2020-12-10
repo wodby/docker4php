ad5163b9cb4099aff207d49329152c62a2c36eb2
Chingis Sandanov <chingis@wodby.com>
2020-12-10 20:29:33 +0700

    Fix tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,7 +6,7 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-git clone https://github.com/bestmomo/laravel5-example.git laravel
+composer create-project laravel/laravel laravel
 cd laravel
 composer install
 php artisan key:generate
@@ -15,9 +15,8 @@ sed -i "s/^DB_DATABASE=.*/DB_DATABASE=${DB_NAME}/" .env
 sed -i "s/^DB_USERNAME=.*/DB_USERNAME=${DB_USER}/" .env
 sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=${DB_PASSWORD}/" .env
 php artisan migrate --seed
-php artisan vendor:publish
 
 sudo chgrp -R www-data storage bootstrap/cache
 sudo chmod -R ug+rwx storage bootstrap/cache
 
-curl -s nginx | grep -q "Laravel 5"
+curl -s nginx | grep -q "Laravel"
