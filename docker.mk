3f2208ac8f31f1dfeb7266f1126d301922388c72
Chingis Sandanov <chingis@wodby.com>
2024-02-27 17:04:21 +0300

    Rename docker-compose to compose

--- docker.mk
+++ docker.mk
@@ -11,8 +11,8 @@ help : docker.mk
 ## up	:	Start up containers.
 up:
 	@echo "Starting up containers for for $(PROJECT_NAME)..."
-	docker-compose pull
-	docker-compose up -d --remove-orphans
+	docker compose pull
+	docker compose up -d --remove-orphans
 
 mutagen:
 	mutagen-compose up
@@ -23,12 +23,12 @@ down: stop
 ## start	:	Start containers without updating.
 start:
 	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
-	@docker-compose start
+	@docker compose start
 
 ## stop	:	Stop containers.
 stop:
 	@echo "Stopping containers for $(PROJECT_NAME)..."
-	@docker-compose stop
+	@docker compose stop
 
 ## prune	:	Remove containers and their volumes.
 ##		You can optionally pass an argument with the service name to prune single container
@@ -36,7 +36,7 @@ stop:
 ##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
 prune:
 	@echo "Removing containers for $(PROJECT_NAME)..."
-	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
+	@docker compose down -v $(filter-out $@,$(MAKECMDGOALS))
 
 ## ps	:	List running containers.
 ps:
@@ -52,7 +52,7 @@ shell:
 ##		logs php	: View `php` container logs.
 ##		logs nginx php	: View `nginx` and `php` containers logs.
 logs:
-	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+	@docker compose logs -f $(filter-out $@,$(MAKECMDGOALS))
 
 # https://stackoverflow.com/a/6273809/1826109
 %:
--- tests/run.sh
+++ tests/run.sh
@@ -6,7 +6,7 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-docker-compose up -d
-docker-compose exec -T mariadb make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
-docker-compose exec -T php tests.sh
-docker-compose down
+docker compose up -d
+docker compose exec -T mariadb make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
+docker compose exec -T php tests.sh
+docker compose down
